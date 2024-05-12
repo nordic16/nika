f859e9b0fe8f37917911a27ede5a1779284f6d27
Diogo <diogo6204@gmail.com>
2024-05-12 17:40:49 +0100

    fixed one issue just to find another

--- src/app/models/comic.tsx
+++ src/app/models/comic.tsx
@@ -7,14 +7,30 @@ export interface Comic {
     source: string
     poster_url: string,
     id: number,
-    poster_path: string
-    
+    poster_path: string,
 }
 
-export default function comic_component(comic: Comic) {    
+export default function ComicComponent({comic}) {  
+    const [img, set_img] = useState('');
+    useEffect(() => {
+        async function fetch_data() {
+            const regex = new RegExp("^.+?[^\/:](?=[?\/]|$)");
+            let referer = regex.exec(comic.source)![0];
+            console.log(referer);
+
+            const headers = {'headers' : {'Referer' : referer}}
+            const response = await fetch(comic.poster_url, headers);
+            const img = URL.createObjectURL(await response.blob());
+
+            set_img(img);
+        }
+        fetch_data();
+    }, []);
+    
     return(
         <div className='relative' key={comic.id}>
             <p className='truncate max-w-64'>{comic.name}</p>
+            <img src={img}></img>
         </div>
     );
 }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,7 +2,7 @@
 import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { montserrat } from '../ui/fonts';
 import { invoke } from '@tauri-apps/api/tauri';
-import comic_component, { Comic } from '../models/comic';
+import ComicComponent, { Comic } from '../models/comic';
 
 const COMICS_DISPLAYED = 12;
 
@@ -15,15 +15,15 @@ export default function Search() {
   function set_page(page: number) {
     s_page(page);
 
-    const comics = get_shown_results();
-
     // Downloads posters for displayed comics...
-    comics.map((comic, i) => {
+   /* comics.map((comic, i) => {
       invoke('download_poster', { comic: comic, source_name: sel_source }).then(path => {
         results[i].poster_path = path as string;
       }).catch(e => console.log(e));
     });
+    */
   }
+  
 
   function handle_search_input(event: FormEvent<HTMLInputElement>) {
     var elem = event.currentTarget.value;
@@ -37,7 +37,7 @@ export default function Search() {
   } 
 
   function handle_source_change(evt: ChangeEvent<HTMLSelectElement>) {
-    const index = evt.target.selectedIndex; // Due to the disabled field.
+    const index = evt.target.selectedIndex;
     set_sel_source(sources[index]);
 
     console.log(`Set source to ${sources[index]}!`);
@@ -64,7 +64,7 @@ export default function Search() {
   }, []);
 
   let options = <select className='text-black p-1' onChange={handle_source_change}>
-    {sources.slice().map((el, index) => <option key={index}>{el}</option>)};
+    {sources.map((el, index) => <option key={index}>{el}</option>)};
   </select>;
 
   let results_div = null;
@@ -74,7 +74,7 @@ export default function Search() {
 
     results_div = <div className='mt-2 bg-nika-secondary p-8 rounded-3xl'>
       <div className='grid grid-cols-4 gap-4 overflow-auto max-h-100'>
-        {comics.map(r => comic_component(r))}
+        {comics.map(r => <ComicComponent comic={r} />)}
       </div>
       <div className='flex gap-8 w-full justify-between mt-6'>
         <button id='-1' onClick={() => load_page(page - 1)} className='text-2xl font-semibold transition ease-in-out hover:text-nika-selected-primary'>Previous</button>
