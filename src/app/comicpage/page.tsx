5fb2e9c6fd7b3b09b19584c56c0094c45e65ba00
Diogo <diogo6204@gmail.com>
2024-06-13 14:58:44 +0100

    changed comic page layout

--- src/app/comicpage/page.tsx
+++ src/app/comicpage/page.tsx
@@ -11,7 +11,8 @@ export default function ComicPage() {
   const params = useSearchParams();
   const comic = JSON.parse(params.get('comic')!) as Comic;    
   const img = params.get('img_path')!;
-
+  
+  const [loading, set_loading] = useState(true);
   const [description, set_description] = useState(String);
 
   useEffect(() => {
@@ -27,35 +28,34 @@ export default function ComicPage() {
       const desc = info.description! as string;
 
       set_description(desc);
-
-      console.log(info.description);
-      
+      set_loading(false);      
     }
 
     get_info();
   });
 
-
-  return (<div className="flex px-12">
-  <img className="rounded-xl w-[300px] md:w-[400px] lg:w-[500px]" src={img} alt={''}></img>
-  <div className="lg:ml-12 ml-8 py-4">
-    <p className={`text-4xl md:text-5xl font-bold ${montserrat.className}`}>{comic?.name}</p>
-    <div className={`flex mt-1 ml-2 gap-2 ${montserrat.className}`}>
-      <p className="py-2 px-4 bg-nika-secondary rounded-xl font-semibold md:text-lg text-[#10B981]">Ongoing</p>
-      <p className="py-2 px-4 bg-nika-secondary rounded-xl font-semibold md:text-lg text-[#FF5555]">Seinen</p>
-      <p className="py-2 px-4 bg-nika-secondary rounded-xl font-semibold md:text-lg text-[#B380FF]">Shonen</p>
+  var body = !loading ? (<div className="flex px-12">
+    <img className="basis-4/12 rounded-xl grow-1" src={img} alt={''}></img>
+    <div className="basis-8/12 lg:ml-12 ml-8 py-4">
+      <p className={`text-4xl md:text-5xl font-bold ${montserrat.className}`}>{comic?.name}</p>
+      <div className={`flex mt-1 ml-2 gap-2 ${montserrat.className}`}>
+        <p className="py-2 px-4 bg-nika-secondary rounded-xl font-semibold md:text-lg text-[#10B981]">Ongoing</p>
+        <p className="py-2 px-4 bg-nika-secondary rounded-xl font-semibold md:text-lg text-[#FF5555]">Seinen</p>
+        <p className="py-2 px-4 bg-nika-secondary rounded-xl font-semibold md:text-lg text-[#B380FF]">Shonen</p>
+      </div>
+  
+      <div className="ml-2 w-full mt-4 bg-nika-secondary hidden xl:block py-4 px-8 rounded-xl">
+        <p className={`${montserrat.className} text-xl md:text-2xl font-bold text-center mb-2`}>Description</p>
+        <p className="">{description}</p>
+      </div>
+  
+      <div className="ml-2 mt-4 bg-nika-secondary py-4 px-8 rounded-xl">
+        <p className={`${montserrat.className} text-xl md:text-2xl font-bold text-center mb-2`}>Chapter List</p>
+      </div>
     </div>
+  
+  
+    </div>) : <div className="relative"><p className={`${montserrat.className} text-4xl text-center  absolute font-extrabold bottom-[50%]`}>Loading...</p></div>
 
-    <div className="ml-2 mt-4 bg-nika-secondary hidden xl:block py-4 px-8 rounded-xl w-full">
-      <p className={`${montserrat.className} text-xl md:text-2xl font-bold text-center mb-2`}>Description</p>
-      <p className="">{description}</p>
-    </div>
-
-    <div className="ml-2 mt-4 bg-nika-secondary py-4 px-8 rounded-xl w-full">
-      <p className={`${montserrat.className} text-xl md:text-2xl font-bold text-center mb-2`}>Chapter List</p>
-    </div>
-  </div>
-
-
-  </div>)
+  return body;
 }
